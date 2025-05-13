40de8a2000d939e66682de724422b84e53603d45
Kriti Gupta <kriti.gupta@tothenew.com>
2025-05-13 16:19:12 +0530

    wip

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/portfolio/', // <-- REQUIRED for GitHub Pages
+  base: '/', // <-- REQUIRED for GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
